392bbf04127f77e898394d8c330b68e856c84bf8
hxperl <hxperl@gmail.com>
2017-12-14 23:54:34 +0900

    Update : post controller test

--- adonisblog/app/Controllers/Http/PostController.js
+++ adonisblog/app/Controllers/Http/PostController.js
@@ -1,8 +1,10 @@
 'use strict'
 
 class PostController {
-    async index() {
-        return 'Posts';
+    async index({ view }) {
+        return view.render('posts.index', {
+            title: 'Latest Posts'
+        })
     }
 }
 
